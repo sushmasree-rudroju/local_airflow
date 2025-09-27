a6d61a2427b2636ca315fd403e31755d330b9090
Your Name <sushmasree.rudroju@gmail.com>
2025-09-27 08:14:40 +0530

    changes made in variables_dag

--- dags/variables_dag.py
+++ dags/variables_dag.py
@@ -6,10 +6,7 @@ def variable_dag():
     @task
     def print_my_var():
         print(Variable.get("api", deserialize_json=True))
-        my_params=Variable.get("my_params")
-        for i in my_params:
-            print(i)
-
+        
     print_my_var()
 
 variable_dag()
