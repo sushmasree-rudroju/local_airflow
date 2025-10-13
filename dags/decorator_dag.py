2abf974f165c6a6304f50632858cc449a864b49c
Your Name <sushmasree.rudroju@gmail.com>
2025-10-13 15:50:02 +0530

    commit Update decorator_dag.py

--- dags/decorator_dag.py
+++ dags/decorator_dag.py
@@ -1,20 +1,26 @@
 from airflow.decorators import dag, task
 from datetime import datetime
+from airflow.operators.python import get_current_context
 
 @dag(start_date=datetime(2022, 1, 1), 
      schedule="@once", catchup=False)
 def decorator_dag():
 
     @task(retries=3)
-    def start():
+    def start(ti=None, ds=None):
+        print(f"Data interval start is {ds}")
+        print(f"TASK INSTANCE IS {ti}")
         return 'success'
     
     @task.branch
     def choose_task(next_task: str):
+        context=get_current_context()
+        print(context)
         return next_task
     
     @task(retries=1)
-    def success():
+    def success(**context):
+        print(context)
         print("Success!")
 
     @task
